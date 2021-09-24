f6a38b24ca94db23e3a1096ee2602f23c9c5b4cb
hum.dev1203@gmail.com <hum.dev1203@gmail.com>
2021-09-24 15:47:27 +0300

    last

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -1,49 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        
-        <?php $this->load->view('commen/css_view'); ?>
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                        m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-            ga('create', 'UA-62546040-1', 'auto');
-            ga('send', 'pageview');
+<head>
+    <meta charset="UTF-8">
 
-        </script>
-    </head>
-    <body>
+    <?php $this->load->view('commen/css_view'); ?>
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-        <?php $this->load->view('commen/header_view'); ?>
+        ga('create', 'UA-62546040-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <style>
+        * {
+            font-family: "Open Sans,Raleway" !important;
 
-        <?php if (!isset($no_heading)): ?>
-            <div class="">
-                <div class="banner clearfix">
-                    <img src="images/<?=$banner?>" class="row-fluid"/>
-                </div>
-                <div class="page_title clearfix bg11">
-                    <h1><span><?= $title ?></span></h1>
-                </div>
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php $this->load->view('commen/header_view'); ?>
+
+    <?php if (!isset($no_heading)) : ?>
+        <div class="">
+            <div class="banner clearfix">
+                <img src="images/<?= $banner ?>" class="row-fluid" />
             </div>
-        <?php endif; ?>
+            <div class="page_title clearfix bg11">
+                <h1><span><?= $title ?></span></h1>
+            </div>
+        </div>
+    <?php endif; ?>
 
-        <?php if (isset($page)): ?>
-            <?php $this->load->view($page); ?>
+    <?php if (isset($page)) : ?>
+        <?php $this->load->view($page); ?>
 
-        <?php endif; ?>
+    <?php endif; ?>
 
 
-        <?php $this->load->view('commen/footer_view'); ?>
+    <?php $this->load->view('commen/footer_view'); ?>
 
-    </body>
+</body>
+
 </html>
