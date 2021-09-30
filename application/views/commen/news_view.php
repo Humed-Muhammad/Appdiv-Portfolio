df0c9082cc0a9f98c89a561c9627126fc5bd0e7b
Humed-Muhammad <abc45171@gmail.com>
2021-09-30 09:00:32 -0700

    last

--- application/views/commen/news_view.php
+++ application/views/commen/news_view.php
@@ -1,5 +1,16 @@
 
 
+<div class="review">
+<div class="row-fluid clearfix">
+    <div class="new_content">
+
+        <p>
+            Our system development company is one of the leading tech enterprise in Ethiopia, we provide different services for our client and we recently launched school system which can replace the old school ways and bring the staffs and students closer and facilitate the learning process and information sharing never been this simple...
+        </p>
+    </div>
+
+</div>
+</div>
     <div class="review">
         <div class="row-fluid clearfix">
             <div class="new_content">
@@ -18,19 +29,21 @@
         <div class="new_content">
 
             <p>
-                We currently Working on big projects that can change peoples life and help them fill the gap between the crazy world of technologies..
+                We currently Working on big projects that can change peoples life and help them fill the gap between them and the crazy world of technologies..
             </p>
         </div>
 
     </div>
     </div>
 
+
+
     <div class="review">
     <div class="row-fluid clearfix">
         <div class="new_content">
 
             <p>
-                We Launched School system.
+               
             </p>
         </div>
 
